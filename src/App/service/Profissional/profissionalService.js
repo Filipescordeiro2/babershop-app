e13de210088666222f4bb1aac51e30886c2211c1
filipescordeiro2 <filipescordeiro@hotmail.com>
2023-10-18 17:03:33 -0300

    .

--- src/App/service/Profissional/profissionalService.js
+++ src/App/service/Profissional/profissionalService.js
@@ -12,37 +12,97 @@ class profissionalService extends ApiService{
     salvarprofissional(profissional){
         return this.post('', profissional)
     }
-    validar(profissional){
+    validar(profissional) {
+        const erros = [];
 
-        const erros =[]
-
-        if(!profissional.nome){
-
-            erros.push('O campo nome é obrigatorio.')
-        }
-        if(!profissional.email){
-
-            erros.push('O campo email é obrigatorio.')
-
-        } else if(!profissional.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
-
-            erros.push('informe um email invalido')
-
-        }
-        if(!profissional.senha || !profissional.senhaRepeticao){
-
-            erros.push('Digite a senha 2x')
-
-        }else if (profissional.senha !== profissional.senhaRepeticao){
-
-            erros.push('As senhas não batem.')
-
-        }
-        if(erros && erros.length>0){
-
+        if (!profissional.nome) {
+            erros.push('O campo nome é obrigatório.');
         }
 
+        if (!profissional.sobrenome) {
+            erros.push('O campo sobrenome é obrigatório.');
+        }
+
+        if (!profissional.cpf) {
+            erros.push('O campo CPF é obrigatório.');
+        } else {
+            const errosCPF = this.validarCPF(profissional.cpf);
+            if (errosCPF.length > 0) {
+                erros.push('CPF inválido.');
+            }
+        }
+
+        if (!profissional.dataDeNascimento) {
+            erros.push('O campo data de nascimento é obrigatório.');
+        }
+
+        if (!profissional.telefone) {
+            erros.push('O campo telefone é obrigatório.');
+        }
+
+        if (!profissional.email) {
+            erros.push('O campo email é obrigatório.');
+        } else if (!profissional.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
+            erros.push('Informe um email inválido.');
+        }
+
+        if (!profissional.senha) {
+            erros.push('Digite a senha');
+        }
+
+        return erros; // Retorna o array de erros ou um array vazio se não houver erros.
     }
+
+
+    validarCPF(cpf) {
+        const erros = [];
+
+        if (!cpf) {
+            erros.push('O campo CPF é obrigatório.');
+        } else {
+            // Remove caracteres não numéricos do CPF
+            cpf = cpf.replace(/[^\d]/g, '');
+
+            // Verifica se o CPF tem 11 dígitos
+            if (cpf.length !== 11) {
+                erros.push('CPF deve conter 11 dígitos.');
+            }
+
+            // Verifica dígitos repetidos
+            if (/^(\d)\1{10}$/.test(cpf)) {
+                erros.push('CPF inválido (todos os dígitos iguais).');
+            }
+
+            // Calcula o primeiro dígito verificador
+            let soma = 0;
+            for (let i = 0; i < 9; i++) {
+                soma += parseInt(cpf.charAt(i)) * (10 - i);
+            }
+            let resto = soma % 11;
+            let digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+            // Verifica o primeiro dígito verificador
+            if (parseInt(cpf.charAt(9)) !== digitoVerificador1) {
+                erros.push('CPF inválido.');
+            }
+
+            // Calcula o segundo dígito verificador
+            soma = 0;
+            for (let i = 0; i < 10; i++) {
+                soma += parseInt(cpf.charAt(i)) * (11 - i);
+            }
+            resto = soma % 11;
+            let digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+            // Verifica o segundo dígito verificador
+            if (parseInt(cpf.charAt(10)) !== digitoVerificador2) {
+                erros.push('CPF inválido.');
+            }
+        }
+
+        return erros;
+    }
+
 
 }
 
---
+++ src/Profissional/view/Cadastro-Profissional.js
@@ -0,0 +1,116 @@
+import React from "react";
+import Card from '../../componets/card'
+import FormGroup from "../../componets/form-group";
+import profissionalService from "../../App/service/Profissional/profissionalService";
+import {withRouter} from "react-router-dom";
+import {mensagemErro, mensagemSucesso} from "../../componets/toastr";
+
+class CadastroProfissional extends React.Component{
+
+    state={
+        nome:'',
+        sobrenome:'',
+        cpf:'',
+        dataDeNascimento:'',
+        telefone:'',
+        email:'',
+        senha:''
+    }
+
+    constructor() {
+        super();
+        this.service = new profissionalService();
+    }
+
+
+    cadastrar = () => {
+        const { nome, sobrenome, cpf, dataDeNascimento, telefone, email, senha } = this.state;
+        const profissional = { nome, sobrenome, cpf, dataDeNascimento, telefone, email, senha };
+
+        const erros = this.service.validar(profissional);
+
+        if (erros.length > 0) {
+            erros.forEach((msg) => mensagemErro(msg));
+            return false;
+        }
+
+        this.service
+            .salvarprofissional(profissional)
+            .then((response) => {
+                mensagemSucesso('Profissional cadastrado com sucesso! Faça o login para acessar');
+                this.props.history.push('/login-profissional');
+            })
+            .catch((error) => {
+                if (error.response && error.response.data) {
+                    mensagemErro(error.response.data);
+                } else {
+                    mensagemErro('Ocorreu um erro ao cadastrar o usuário');
+                }
+            });
+    }
+
+
+    cancelar = ()=>{
+        this.props.history.push('/')
+    }
+
+    render() {
+        return(
+            <Card title="Cadastro Profissional">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="bs-component">
+                            <FormGroup label="Nome: *" htmlFor="inputNome">
+                                <input type="text" id="inputNome"
+                                       className="form-control" name="nome"
+                                       onChange={e=>this.setState({nome:e.target.value
+                                })}/>
+                            </FormGroup>
+                            <FormGroup label="Sobrenome: *" htmlFor="inputSobrenome">
+                                <input type="text" id="inputSobrenome"
+                                       className="form-control" name="sobrenome"
+                                       onChange={e=>this.setState({sobrenome:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <FormGroup label="CPF: *" htmlFor="inputCPF">
+                                <input type="text" id="inputCPF"
+                                       className="form-control" name="cpf"
+                                       onChange={e=>this.setState({cpf:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <FormGroup label="Data de Nascimento: *" htmlFor="inputDataDeNascimento">
+                                <input type="date" id="inputDataDeNascimento"
+                                       className="form-control" name="dataDeNascimento"
+                                       onChange={e=>this.setState({dataDeNascimento:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <FormGroup label="Telefone: *" htmlFor="inputTelefone">
+                                <input type="text" id="inputTelefone"
+                                       className="form-control" name="telefone"
+                                       onChange={e=>this.setState({telefone:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <FormGroup label="Email: *" htmlFor="inputEmail">
+                                <input type="text" id="inputEmail"
+                                       className="form-control" name="email"
+                                       onChange={e=>this.setState({email:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                <input type="password" id="inputSenha"
+                                       className="form-control" name="senha"
+                                       onChange={e=>this.setState({senha:e.target.value
+                                       })}/>
+                            </FormGroup>
+                            <br/>
+                            <button onClick={this.cadastrar} type="button" className="btn btn-success">Cadastrar</button>
+                            <button onClick={this.cancelar} type="button" className="btn btn-danger">Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            </Card>
+
+        )
+    }
+}
+export default withRouter(CadastroProfissional)
